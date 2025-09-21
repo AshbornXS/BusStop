b75c10d2dd8e199e6cf85991d0c3330e8e140c19
AshbornXS <brutalivio4@gmail.com>
2025-09-21 17:07:30 -0300

    refactor: move server initialization from server.js to app.js and update dependencies

--- src/app.js
+++ src/app.js
@@ -6,10 +6,15 @@ import locationRoutes from "./routes/locationRoutes.js";
 dotenv.config();
 connectDB();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.use(express.json());
 
 // Rotas
 app.use("/api/locations", locationRoutes);
 
-export default app;
+// Iniciando o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
