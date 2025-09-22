5eff7a3e91b2458882283663b6c5978cbd1efb14
AshbornXS <brutalivio4@gmail.com>
2025-09-21 23:21:53 -0300

    feat: add start script to package.json and implement test route in app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
+// Rota teste
+app.get("/", (req, res) => {
+  res.send("🚍 BuStop API rodando!");
+});
+
 // Rotas
 app.use("/api/locations", locationRoutes);
 
