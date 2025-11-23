4dba9ff13cfe486988f744a286a2b2065017851b
AshbornXS <brutalivio4@gmail.com>
2025-11-23 19:48:44 -0300

    refactor: simplificar configuração do CORS removendo opções explícitas

--- src/app.js
+++ src/app.js
@@ -13,14 +13,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-// Configuração explícita do CORS para permitir DELETE e Headers de Auth
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
+app.use(cors());
 app.use(express.json());
 
 // Rota teste
